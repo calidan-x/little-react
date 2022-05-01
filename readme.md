0ec3c18cb96d693c2a686280d317ffa12651e9c6
Calidan <calidan@vip.qq.com>
2022-05-01 21:29:34 +0800

    docs: 添加readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+### 超级简单的 React 简版本
+
+- 自持 tsx
+- 支持 useState
+
+执行 npm run compile 编译<br/>
+使用浏览器打开 index.html
--- src/react.tsx
+++ src/react.tsx
@@ -1,56 +1,56 @@
 // 完成简单的Virtual DOM
-const componentStates: any = {};
+const componentStates: any = {}
 
-let componentId = '';
-let componentIdCounter = 1;
-let stateCounter = 0;
+let componentId = ''
+let componentIdCounter = 1
+let stateCounter = 0
 
 function useState<T>(initVal: T) {
   if (!componentStates[componentId]) {
-    componentStates[componentId] = [];
+    componentStates[componentId] = []
   }
 
-  const componentState = componentStates[componentId];
+  const componentState = componentStates[componentId]
 
   if (componentState.length <= stateCounter) {
-    componentState[stateCounter] = initVal;
+    componentState[stateCounter] = initVal
   }
 
-  const val = componentState[stateCounter];
+  const val = componentState[stateCounter]
 
   // 缓存stateCounter的高阶函数
   const setVal = ((c: number) => (val: T) => {
-    componentState[c] = val;
-    ReactDOM.refresh();
-  })(stateCounter);
+    componentState[c] = val
+    ReactDOM.refresh()
+  })(stateCounter)
 
-  stateCounter++;
-  return [val, setVal] as [T, (val: T) => void];
+  stateCounter++
+  return [val, setVal] as [T, (val: T) => void]
 }
 
 const React = {
   createElement(type, props, ...children) {
-    return { type, props, children };
+    return { type, props, children }
   }
-};
+}
 
 const generateDOM = (vElement: any) => {
-  const { type, props, children } = vElement;
-  let domElem = null;
+  const { type, props, children } = vElement
+  let domElem = null
   // 是组件
   if (typeof type === 'function') {
-    componentId = 'Comp' + componentIdCounter;
-    componentIdCounter++;
-    stateCounter = 0;
+    componentId = 'Comp' + componentIdCounter
+    componentIdCounter++
+    stateCounter = 0
     // 传入组件属性
-    domElem = generateDOM(type(props));
+    domElem = generateDOM(type(props))
   }
   // 是原生html标签
   else {
-    domElem = document.createElement(type);
+    domElem = document.createElement(type)
     if (props) {
       for (const p in props) {
-        domElem[p.toLowerCase()] = props[p];
+        domElem[p.toLowerCase()] = props[p]
       }
     }
   }
@@ -60,54 +60,54 @@ const generateDOM = (vElement: any) => {
     children.forEach((c) => {
       if (c) {
         if (typeof c !== 'object') {
-          domElem.appendChild(document.createTextNode(c));
+          domElem.appendChild(document.createTextNode(c))
         } else {
-          domElem.appendChild(generateDOM(c));
+          domElem.appendChild(generateDOM(c))
         }
       }
-    });
+    })
   }
-  return domElem;
-};
+  return domElem
+}
 
 const ReactDOM = {
   virtualDOM: {},
   rootElement: null,
   refresh() {
-    componentIdCounter = 1;
-    this.rootElement.innerHTML = '';
-    this.rootElement.appendChild(generateDOM(this.virtualDOM));
+    componentIdCounter = 1
+    this.rootElement.innerHTML = ''
+    this.rootElement.appendChild(generateDOM(this.virtualDOM))
   },
   render(elements: any, domElem: HTMLElement) {
-    this.virtualDOM = elements;
-    this.rootElement = domElem;
-    this.refresh();
+    this.virtualDOM = elements
+    this.rootElement = domElem
+    this.refresh()
   }
-};
+}
 
 function Comp({ text }: { text: string }) {
-  const [title, setTitle] = useState('点击标题');
-  const [count, setCount] = useState(1);
+  const [title, setTitle] = useState('点击标题')
+  const [count, setCount] = useState(1)
 
   return (
     <div>
       {text}
       <div
         onClick={() => {
-          setTitle('标题改变');
+          setTitle('标题改变')
         }}
       >
         {title}
       </div>
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount(count + 1)
         }}
       >
         点击：{count}
       </button>
     </div>
-  );
+  )
 }
 
 // 因为没有生成 Virtual DOM 所以第一个参数只能传入一个重新渲染的函数
@@ -115,7 +115,7 @@ ReactDOM.render(
   <div>
     <button
       onClick={() => {
-        alert('点击事件');
+        alert('点击事件')
       }}
     >
       点击事件
@@ -130,4 +130,4 @@ ReactDOM.render(
     <br />
   </div>,
   document.body
-);
+)
